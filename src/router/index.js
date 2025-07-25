fa67625eff986a1e6500cf0d4abc058eed4ecad7
zmoon <z_moon789@outlook.com>
2025-07-25 20:29:54 +0800

    fix(router): 将根路径重定向从/goods改为/dashboard

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 const routes = [
-  { path: '/', redirect: '/goods' },
+  { path: '/', redirect: '/dashboard' },
   { path: '/dashboard', component: () => import('../views/Dashboard.vue') },
   { path: '/goods',     component: () => import('../views/GoodsList.vue') },
   { path: '/orders',    component: () => import('../views/OrderList.vue') },
